03f2bfd31fedd20bc506a1005ba7d2c6d76ed595
farshid92 <farshidcheraghchian@gmail.com>
2023-02-01 21:44:39 +0200

    first commit DatingApp

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Farshid");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "John");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Zack");
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+using System.Security.AccessControl;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        
+        public string UserName { get; set; }
+    }
+}
